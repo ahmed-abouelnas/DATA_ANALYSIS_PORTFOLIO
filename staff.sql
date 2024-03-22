c2418779a1eddf1ed8a936a557b88939250860a8
ahmed-abouelnas <164519061+ahmed-abouelnas@users.noreply.github.com>
2024-03-22 14:42:42 +0000

    Add files via upload

---
+++ staff.sql
@@ -0,0 +1,16 @@
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st001', 'Mindy', 'Sloan', 'Chef', 17.25);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st002', 'Luqman', 'Cantu', 'Head Chef', 21.50);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st003', 'Seren', 'Lindsey', 'Chef', 17.25);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st004', 'Arran', 'Hodgson', 'Head Chef', 21.50);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st005', 'Talha', 'Portillo', 'Chef', 17.25);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st006', 'Sana', 'Black', 'Head Chef', 21.50);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st007', 'Zachary', 'Robins', 'Chef', 17.25);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st008', 'Faraz', 'Peck', 'Head Chef', 21.50);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st009', 'Lilly-Rose', 'Vaughn', 'Delivery rider', 14.50);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st010', 'Desiree', 'Gardner', 'Delivery rider', 14.50);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st011', 'Ivan', 'English', 'Delivery rider', 14.50);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st012', 'Johnathon', 'Bradford', 'Delivery rider', 14.50);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st013', 'Matilda', 'Mccarty', 'Delivery rider', 14.50);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st014', 'Areeb', 'Vasquez', 'Delivery rider', 14.50);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st015', 'Amiyah', 'Lambert', 'Delivery rider', 14.50);
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `posiiton`, `hourly_rate`) VALUES ('st016', 'Amrit', 'Greaves', 'Delivery rider', 14.50);
